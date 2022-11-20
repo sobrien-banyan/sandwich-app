e652796b6b4cf7db113b80513ba99ab6eb4bd157
sobrien-banyan <sobrien@banyanlabs.io>
2022-11-20 09:05:25 -0700

    sandwich generator

--- sandwich_server/sandwich_app/urls.py
+++ sandwich_server/sandwich_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-# from sandwich_app.views import SandwichappView, IngredientsView
-from sandwich_app.views import SandwichappView, IngredientsView
+
+from sandwich_app.views import SandwichappView, IngredientsView, SandwichGeneratorView
 
 urlpatterns = [
     path('', SandwichappView.as_view(), name='sandwich'),
     path('ingredients/<str:ingredient_type>', IngredientsView.as_view(), name='ingredient_type'),
+    path('random', SandwichGeneratorView.as_view(), name='sandwich_generator')
 ]
--- sandwich_server/sandwich_app/views.py
+++ sandwich_server/sandwich_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.views import View
-
+import random
 # Create your views here.
 
 ingredients = {
-    'meats': ['beef', 'turkey'],
-    'cheeses': ['chedder', 'sharp chedder'],
-    'toppings': ['mayo', 'mustard'],
+    'meats': ['corned beef', 'pastrami', 'honey turkey', 'pepper steak', 'veggie burger'],
+    'cheeses': ['american', 'swiss', 'provolone', 'cheddar', 'mozzarella'],
+    'toppings': ['lettuce', 'tomato', 'onions', 'peppers', 'pickles']
 }
 
 class SandwichappView(View):
@@ -19,11 +20,23 @@ class SandwichappView(View):
 
 class IngredientsView(View):
     def get(self, request, ingredient_type):
-        return render (
+        if ingredient_type not in ingredients:
+            raise Http404(f'No such ingredient: {ingredient_type}')
+
+        return render(
             request = request,
-            template_name='ingredients_list.html',
-            context={
+            template_name = 'ingredients_list.html',
+            context = {
                 'ingredients': ingredients[ingredient_type],
-                'ingredient': ingredient_type
+                'ingredient_type': ingredient_type,
             }
         )
+
+class SandwichGeneratorView(View):
+    def get(self, request):
+            selected_meat = random.choice(ingredients['meats'])
+            selected_cheese = random.choice(ingredients['cheeses'])
+            selected_toppings = random.choice(ingredients['toppings'])
+
+            sandwich = f'{selected_meat} & {selected_cheese} with {selected_toppings}'
+            return render(request, 'sandwich_generator.html', context = { 'sandwich' : sandwich})
